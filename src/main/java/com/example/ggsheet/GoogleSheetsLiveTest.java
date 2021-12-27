8b58f2f3b6849e5fb890d514fa4111dfc8b6eeb9
LDEV <phamlinh1998>
2021-12-27 10:52:00 +0000

    fix

--- src/main/java/com/example/ggsheet/GoogleSheetsLiveTest.java
+++ src/main/java/com/example/ggsheet/GoogleSheetsLiveTest.java
@@ -1,6 +1,7 @@
 package com.example.ggsheet;
 
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import org.springframework.stereotype.Service;
@@ -16,18 +17,26 @@ public class GoogleSheetsLiveTest {
   //  @BeforeClass
   public static void setup() throws GeneralSecurityException, IOException {
     Sheets sheetsService = SheetsServiceUtil.getSheetsService();
+//    ValueRange body = new ValueRange().setValues(Arrays.asList(
+//            Collections.singletonList("Expenses January"),
+//            Arrays.asList("books1", "30"),
+//            Arrays.asList("pens1", "10"),
+//            Collections.singletonList("Expenses February"),
+//            Arrays.asList("clothes", "20"),
+//            Arrays.asList("shoes", "5")));
     ValueRange body = new ValueRange().setValues(Arrays.asList(
-            Collections.singletonList("Expenses January"),
-            Arrays.asList("books", "30"),
-            Arrays.asList("pens", "10"),
-            Collections.singletonList("Expenses February"),
-            Arrays.asList("clothes", "20"),
-            Arrays.asList("shoes", "5")));
+            //Arrays.asList("Expenses January", "price"),
+            Arrays.asList("books1", "30"),
+            Arrays.asList("pens111111", "10")));
     // ...
     String SPREADSHEET_ID = "14cFRQQdeXiFbk2r_QIRU9g7DugZzO7tdcdIv5EJADMs";
-    UpdateValuesResponse result = sheetsService.spreadsheets().values()
-            .update(SPREADSHEET_ID, "A1", body)
-            .setValueInputOption("RAW")
+//    UpdateValuesResponse result = sheetsService.spreadsheets().values()
+//            .update(SPREADSHEET_ID, "A15", body)
+//            .setValueInputOption("RAW")
+//            .execute();
+    AppendValuesResponse response = sheetsService.spreadsheets().values().append(SPREADSHEET_ID, "A1", body).setValueInputOption("USER_ENTERED")
+            .setInsertDataOption("INSERT_ROWS")
+            .setIncludeValuesInResponse(true)
             .execute();
   }
 
